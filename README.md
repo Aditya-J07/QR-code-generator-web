9c19138d214f9dbf0df3495b56ab6d20451b51a5
Aditya Jha <adityajha.vats02@gmail.com>
2026-05-14 17:15:42 +0530

    qr

---
+++ README.md
@@ -0,0 +1,24 @@
+# Flask QR Code Generator
+
+A simple QR Code Generator web application built using Flask and Python.
+
+# Installation
+
+Clone the repository
+Create virtual environment
+Install required packages
+
+```bash
+pip install requirment.txt
+```
+Run the Application
+
+```bash
+python app.py
+```
+
+Open browser:
+
+```plaintext
+http://127.0.0.1:5000
+```
---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, send_file
+import qrcode as qr
+import os
+
+app = Flask(__name__)
+
+
+
+@app.route('/',methods=['POST', 'GET'])
+
+def home():
+    image_generated=False
+
+    file_path = "static/qrcode.png"
+
+    if request.method == 'POST':
+
+        url= request.form['content']
+
+        qrcode = qr.QRCode()
+        qrcode.add_data(url)
+        qrcode.make(fit=True)
+
+        img = qrcode.make_image()
+        img.save(file_path)
+        image_generated=True
+
+    return render_template("index.html", image_generated=image_generated)
+
+@app.route("/download")
+def download():
+    return send_file("static/qrcode.png", as_attachment=True)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
